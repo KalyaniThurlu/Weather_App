93ca7233ff1ca3050b682d90f9eed0047262def6
KalyaniThurlu <kalyanithurlu@gmail.com>
2024-10-09 21:16:46 +0530

    KalyaniThurlu | removing white space

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+
+
 import './App.css';
 import { Home } from './components/weather';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   return (
-    <div>
+    <div className="App">
      
      <Home/>
     </div>
